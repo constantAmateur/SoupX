04f90602fc12687ec49a9b598b1f05c1d22f24d5
Matthew Young <my4@sanger.ac.uk>
2020-05-05 23:58:07 +0100

    Split data into one RData file per object as required.  Enable lazy loading.

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
-#' PBMC 4K data
+#' PBMC 4K meta data
 #'
-#' Collection of bits of data relating to the 10X PBMC 4K data.
+#' Collection of bits of meta data relating to the 10X PBMC 4K data.
 #' 
-#' This data set pertains to the 10X demonstration PBMC 4K data and includes 2 objects: \code{PBMC_DR} and \code{PBMC_cellBarcodes}
+#' This data set pertains to the 10X demonstration PBMC 4K data and includes metadata about it in the \code{data.frame} named \code{PBMC_DR}.
 #'
 #' \code{PBMC_DR} was created using Seurat (v2) to calculate a tSNE representation of the data and cluster cells with these commands.
 #' \itemize{
@@ -20,11 +20,22 @@
 #'   \item \code{PBMC_DR$Annotation = factor(c('7'='B','4'='B','1'='T_CD4','2'='T_CD4','3'='T_CD8','5'='T_CD8','6'='NK','8'='NK','0'='MNP','9'='MNP','10'='MNP','11'='?')[as.character(PBMC_DR$Cluster)])}
 #' }
 #' 
-#' \code{PBMC_cellBarcodes} was created with \code{PBMC_cellBarcodes = colnames(Read10X('filtered_gene_bc_matrices/GRCh38/'))}.
-#' 
-#' @format \code{PBMC_DR} is a data.frame with 4 columns: RD1, RD2, Cluster, and Annotation.  \code{PBMC_cellBarcodes} is a vector containing the barcodes for droplets containing cells.
-#' @usage data(PBMC)
-#' @name PBMC
+#' @format \code{PBMC_DR} is a data.frame with 4 columns: RD1, RD2, Cluster, and Annotation.
+#' @usage data(PBMC_DR)
+#' @name PBMC_DR
 #' @docType data
 #' @source \url{https://support.10xgenomics.com/single-cell-gene-expression/datasets/2.1.0/pbmc4k}
-NULL
+"PBMC_DR"
+
+#' PBMC 4K cellular barcodes
+#'
+#' A vector of cellular barcodes
+#' 
+#' This vector was created by running \code{PBMC_cellBarcodes = colnames(Seurat::Read10X('filtered_gene_bc_matrices/GRCh38/'))}.
+#'
+#' @format \code{PBMC_cellBarcodes} is a vector containing the barcodes for droplets containing cells.
+#' @usage data(PBMC_cellBarcodes)
+#' @name PBMC_cellBarcodes
+#' @docType data
+#' @source \url{https://support.10xgenomics.com/single-cell-gene-expression/datasets/2.1.0/pbmc4k}
+"PBMC_cellBarcodes"
